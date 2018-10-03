c758f569b623ab55c6d38ba96585c1ba75b361c6
Emelie <34678482+emelliie@users.noreply.github.com>
2018-10-03 21:32:15 +0200

    La till klassen

---
+++ src/JDBCTest.java
@@ -0,0 +1,40 @@
+import java.sql.*;
+import java.sql.DriverManager;
+
+public class JDBCTest {
+	public static void main(String[] args)  {
+		// TODO Auto-generated method stub
+try {
+	Class.forName("com.microsoft.sqlserver.jdbc.SQLserverDriver");
+} catch (ClassNotFoundException e) {
+		e.printStackTrace();
+	}
+	
+	String url = "jdbc:sqlserver://localhost:1433;database=Assignment2;";
+	String user = "EmelieJavaSQL";	
+	String pass = "panda3"; 
+	
+	
+	try {
+	
+	
+	Connection con = DriverManager.getConnection(url, user, pass);
+	String query = "SELECT * FROM DEMO DATATBASE NAV (5-0);";
+	PreparedStatement ps = con.prepareStatement(query);
+	 
+	
+	ResultSet rs = ps.executeQuery();
+	
+	while(rs.next()) {
+		System.out.println(rs.getString(1));
+	}
+	
+	}
+  
+	catch(SQLException e) {
+	e.printStackTrace();
+	
+    }
+	
+   }
+}
